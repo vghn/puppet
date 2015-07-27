d86fc71a86869705ae7ad368ed8cd6664cbb0525
Vlad Ghinea <vlad@ghn.me>
2015-07-27 10:58:14 -0500

    Change provision script

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-#
-# wget -qO- https://raw.githubusercontent.com/vladgh/puppet/master/provision/bootstrap.sh | bash
+# This script installs and configures Puppet
+# USAGE:
+#   export ENV=production
+#   wget -qO- "https://raw.githubusercontent.com/vladgh/puppet/${ENV}/provision/bootstrap.sh" | bash
 
 set -e
 
 # VARs
-puppet='/opt/puppetlabs/bin/puppet'
-r10k='/opt/puppetlabs/puppet/bin/r10k'
-provision='https://raw.githubusercontent.com/vladgh/puppet/testing/provision'
+ENV=${ENV:-production}
 
 # Load Functions
 echo 'Loading remote functions'
@@ -17,20 +17,28 @@ echo 'Loading remote functions'
 load_remote_scripts aws git puppet
 aws_install_cli
 git_install_latest
-puppet_install_agent;
+puppet_install_agent
+puppet='/opt/puppetlabs/bin/puppet'
 
 # Initial resources
 $puppet resource service puppet ensure=stopped enable=false
 $puppet resource service mcollective ensure=stopped enable=false
 $puppet resource package r10k ensure=latest provider=puppet_gem
+r10k='/opt/puppetlabs/puppet/bin/r10k'
 
 # Get configuration files
+echo 'Getting r10k and hiera configurations'
+provision="https://raw.githubusercontent.com/vladgh/puppet/${ENV}/provision"
 mkdir -p /etc/puppetlabs/{code,r10k}
-wget -qO /etc/puppetlabs/r10k/r10k.yaml "${provision}/r10k.yaml"
-wget -qO /etc/puppetlabs/code/hiera.yaml "${provision}/hiera.yaml"
+wget -O /etc/puppetlabs/r10k/r10k.yaml "${provision}/r10k.yaml"
+wget -O /etc/puppetlabs/code/hiera.yaml "${provision}/hiera.yaml"
 
 # Deploy Puppet environments
-$r10k --verbose deploy environment --puppetfile --verbose
+$r10k deploy environment --puppetfile --verbose
+
+# Apply Puppet
+$puppet apply "/etc/puppetlabs/code/environments/${ENV}/manifests/site.pp" \
+  --environment "$ENV"
 
 # DONE
 e_finish
