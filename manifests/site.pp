dba90de932fada11dda1f75d057c645772467101
Vlad Ghinea <vlad@ghn.me>
2015-07-27 10:57:50 -0500

    Add defaults in main manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,20 @@
+# PRIMARY FILEBUCKET
+# This configures puppet agent and puppet inspect to back up file contents when
+# they run.
+
+# Define filebucket 'main'
+filebucket { 'main':
+  server => $::servername,
+  path   => false,
+}
+
+# Make filebucket 'main' the default backup location for all File resources
+File { backup => 'main' }
+
+# Exec defaults:
+Exec { path => '/usr/local/bin:/usr/bin:/usr/sbin/:/bin:/sbin' }
+
+# DEFAULT NODE
 node default {
   hiera_include('classes')
 }
