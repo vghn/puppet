966825eed0cfabe0d67b47842ec893f0e589b4c5
Vlad Ghinea <vlad@ghn.me>
2015-08-05 21:31:24 -0500

    Fix r10k postrun script

--- provision/r10k_postrun.sh
+++ provision/r10k_postrun.sh
@@ -6,15 +6,19 @@
 #     `s3://myBucket/production` into `/etc/puppetlabs/code/environments/production`
 
 ENV_DIR='/etc/puppetlabs/code/environments'
-AWS_ZONE=$(wget --timeout=3 -qO- 'http://instance-data/latest/meta-data/placement/availability-zone')
-AWS_REGION="${AWS_ZONE%?}"
-AWS_REGION="${AWS_REGION:-us-east-1}"
+
+if [ ! -z "$AWS_REGION" ]; then
+  AWS_ZONE=$(wget --timeout=3 -qO- 'http://instance-data/latest/meta-data/placement/availability-zone')
+  AWS_REGION="${AWS_ZONE%?}"
+  AWS_REGION="${AWS_REGION:-us-east-1}"
+fi
 
 if [[ "${PRIVATE_DATA}" =~ ^s3:// ]]; then
   for dir in ${ENV_DIR}/*; do
     [[ -d $dir ]] || break
     name=$(basename "$dir")
     priv="${dir}/hieradata/private"
+    mkdir -p "$priv"
     /usr/local/bin/aws --region "$AWS_REGION" s3 sync \
       "${PRIVATE_DATA}/${name}/" "${priv}/" --delete || break
   done
