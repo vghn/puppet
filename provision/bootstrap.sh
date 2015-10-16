e770f1d68b82c6369d83d38ce3374c3b1a364948
Vlad Ghinea <vlad@ghn.me>
2015-10-16 11:32:25 -0500

    Add bootstrap script

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -1,121 +1,69 @@
 #!/usr/bin/env bash
 #
-# REQUIRES:
-# sudo apt-get -qy install wget
+# This scripts installs the latest Puppet Agent.
 #
-# USAGE:
-# bash <(wget -qO- https://raw.githubusercontent.com/vladgh/puppet/master/provision/bootstrap.sh) --role=myrole --env=production
+# wget -qO- https://raw.githubusercontent.com/vladgh/puppet/master/provision/puppet.sh | bash
 
-# Immediately exit on error, including inside a chain of pipes.
-set -o errexit
-set -o nounset
-set -o pipefail
+# Immediately exit on errors
+set -euo pipefail
 
-# DEFAULTS
-ROLE='none'
-ENVIRONMENT='production'
-PUPPET_COLLECTION='pc1'
-GIT_USER='vladgh'
-GIT_REPO='puppet'
+# START ########################################################################
+echo "Puppet install script started at $(/bin/date "+%F %T")"
 
-# Parse command line arguments
-for var in "$@"; do
-  if [[ "$var" =~ --role=.* ]]; then
-    ROLE=${var//--role=/}
-  elif [[ "$var" =~ --env=.* ]]; then
-    ENVIRONMENT=${var//--env=/}
-  fi
-done
-
-# VARs
-RAW_URL="https://raw.githubusercontent.com/${GIT_USER}/${GIT_REPO}/${ENVIRONMENT}"
-PROVISION_URL="${RAW_URL}/provision"
+# DEFAULTS #####################################################################
+COLLECTION='pc1'
 PATH=/opt/puppetlabs/puppet/bin:/opt/puppetlabs/puppet/bin:$PATH
 TMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmp')
-DISTRIBUTION=$(lsb_release -cs)
-SHOULD_UPDATE_APT=false
-PUPPET_DIR=/etc/puppetlabs
-R10K_DIR="${PUPPET_DIR}/r10k"
-CODE_DIR="${PUPPET_DIR}/code"
 
+# FUNCTIONS ####################################################################
 
-# Check if command exists
+## Command exists
 is_cmd() { command -v "$@" >/dev/null 2>&1 ;}
-# APT install package
-apt_install(){ echo "Installing $*" && sudo apt-get -qy install "$@" < /dev/null ;}
+
+## Release is Ubuntu
+is_ubuntu() { is_cmd lsb_release && [[ "$(lsb_release -si)" =~ Ubuntu ]] ;}
+
+## Get codename
+codename() { is_cmd lsb_release && lsb_release -cs ;}
+
+## Update APT
 apt_update() { echo 'Updating APT' && sudo apt-get -qy update < /dev/null ;}
-apt_upgrade(){ echo 'Upgrading system' && sudo apt-get -qy upgrade < /dev/null ;}
 
-# Update/upgrade system
-apt_update && apt_upgrade
+## Install package
+apt_install() {
+  apt_update
+  echo "Installing $*" && sudo apt-get -qy install "$@" < /dev/null
+}
 
-# Install apt utilities
+## Upgrade system
+apt_upgrade() {
+  apt_update
+  echo 'Upgrading system' && sudo apt-get -qy upgrade < /dev/null
+}
+
+# INSTALL PUPPET  ##############################################################
+
+## Make sure wget is installed (even though it should be, if we execute this
+## remotely)
+is_cmd wget || apt_install wget
+
+## Make sure we can add the repository
 is_cmd add-apt-repository || \
   apt_install python-software-properties software-properties-common
 
-# Install Puppet release package
+## Install Puppet release package
 if is_cmd puppet; then
   echo 'Puppet already installed'
   puppet --version
 else
   echo 'Installing Puppet release package'
-  remote_deb="puppetlabs-release-${PUPPET_COLLECTION}-${DISTRIBUTION}.deb"
-  local_deb="${TMPDIR}/${remote_deb}"
-  wget -qO "$local_deb" "https://apt.puppetlabs.com/${remote_deb}"
-  sudo dpkg -i "$local_deb"
-  SHOULD_UPDATE_APT=true
+  debname="puppetlabs-release-${COLLECTION}-$(codename).deb"
+  debfile="${TMPDIR}/${debname}"
+  wget -qO "$debfile" "https://apt.puppetlabs.com/${debname}"
+  sudo dpkg -i "$debfile"
+  apt_install puppet-agent
 fi
 
-# Install GIT official Launchpad PPA repository
-if [ ! -s "/etc/apt/sources.list.d/git-core-ppa-${DISTRIBUTION}.list" ]; then
-  echo 'Adding the official Launchpad repository for GIT'
-  sudo add-apt-repository -y 'ppa:git-core/ppa'
-  SHOULD_UPDATE_APT=true
-fi
-
-# Install packages (update APT if necessary)
-$SHOULD_UPDATE_APT && apt_update
-apt_install \
-  git \
-  puppet-agent \
-  python-pip \
-  python-dev \
-  libssl-dev \
-  libffi-dev \
-  curl
-
-# Upgrade PIP and install AWS CLI
-echo 'Installing PIP packages'
-sudo -H pip install --upgrade \
-  pip setuptools pyopenssl ndg-httpsclient pyasn1 awscli
-
-# Install r10k
-echo 'Installing r10k'
-is_cmd r10k || puppet resource package r10k ensure=latest provider=puppet_gem
-
-# Get initial Puppet configuration
-echo 'Get initial Puppet configuration'
-mkdir -p "${CODE_DIR}"
-mkdir -p "${R10K_DIR}"
-wget -O "${CODE_DIR}/hiera.yaml" "${PROVISION_URL}/hiera.yaml"
-wget -O "${R10K_DIR}/r10k.yaml" "${PROVISION_URL}/r10k.yaml"
-
-# Set Facter facts
-FACTS_DIR=/etc/puppetlabs/facter/facts.d
-mkdir -p $FACTS_DIR
-echo "role: ${ROLE}" > "${FACTS_DIR}/role.yaml"
-
-# R10K Deployment
-echo 'Deploy R10k environments'
-r10k deploy environment --puppetfile --verbose
-
-# Apply Puppet
-echo 'Apply Puppet'
-puppet apply \
-  --detailed-exitcodes \
-  --verbose \
-  --environment="${ENVIRONMENT}" \
-  "${CODE_DIR}/environments/${ENVIRONMENT}/manifests/site.pp"
-
-echo "Bootstrap finished at $(/bin/date "+%F %T")"
+# FINISH #######################################################################
+echo "Puppet install script finishedat $(/bin/date "+%F %T")"
 
