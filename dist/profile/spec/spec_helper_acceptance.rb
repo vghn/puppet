7a95a7e5821056d70807399efdffd2fd89cfc00e
Vlad Ghinea <vlad@ghn.me>
2015-11-20 19:04:59 -0600

    Replace sip with copy_module_to
    copy_module_to already has ignore patterns

--- dist/profile/spec/spec_helper_acceptance.rb
+++ dist/profile/spec/spec_helper_acceptance.rb
@@ -3,7 +3,7 @@ require 'beaker-rspec/helpers/serverspec'
 
 RSpec.configure do |c|
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  fixture_modules = File.join(proj_root, 'spec', 'fixtures', 'modules')
+  fixture_modules_dir = File.join(proj_root, 'spec', 'fixtures', 'modules')
 
   # Readable test descriptions
   c.formatter = :documentation
@@ -11,25 +11,18 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     hosts.each do |host|
-      # copies all the fixtures over
-      scp_to host,
-             fixture_modules,
-             '/etc/puppetlabs/code',
-             ignore: [
-               '.bundle',
-               '.git',
-               '.idea',
-               '.vagrant',
-               '.vendor',
-               'vendor',
-               'acceptance',
-               'bundle',
-               'spec',
-               'tests',
-               'log',
-               '.',
-               '..'
-             ]
+      # SCP modules folder (this excludes stuff like spec and test folders)
+      fixture_modules = Dir["#{fixture_modules_dir}/*/"].map do |mod|
+        File.basename(mod)
+      end
+      fixture_modules.each do |module_name|
+        module_dir = "#{fixture_modules_dir}/#{module_name}"
+        copy_module_to(
+          host,
+          source: module_dir,
+          module_name: module_name
+        )
+      end
 
       # Install this module, this is required because symlinks are not
       # transferred in the step above
