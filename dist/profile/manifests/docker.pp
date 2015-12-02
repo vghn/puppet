19f95f7d248c9b3e6cfa56d45814129eb38d6905
Vlad Ghinea <vlad@ghn.me>
2015-12-02 07:35:56 -0600

    Fix fact scope

--- dist/profile/manifests/docker.pp
+++ dist/profile/manifests/docker.pp
@@ -2,7 +2,7 @@
 class profile::docker {
   include ::profile::base
 
-  if $virtual == 'docker' {
+  if $::virtual == 'docker' {
     warning('Docker in Docker is not yet supported!')
   } else {
     class {'::docker': package_key_source => 'http://apt.dockerproject.org/gpg'}
