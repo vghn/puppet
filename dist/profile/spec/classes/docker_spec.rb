55af79f6a27451960dc693016b2d4732cea5a610
Vlad Ghinea <vlad@ghn.me>
2015-12-05 08:33:06 -0600

    Add more tests for the docker class

--- dist/profile/spec/classes/docker_spec.rb
+++ dist/profile/spec/classes/docker_spec.rb
@@ -9,8 +9,15 @@ describe 'profile::docker' do
         end
 
         it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('profile::docker') }
         it { is_expected.to contain_class('profile::base') }
         it { is_expected.to contain_class('docker') }
+
+        it { is_expected.to contain_package('apparmor') }
+        it { is_expected.to contain_package('apt-transport-https') }
+        it { is_expected.to contain_package('cgroup-lite') }
+
+        it { is_expected.to contain_apt__pin('docker') }
       end
     end
   end
