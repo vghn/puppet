23a798639cf64979492d8d075033eb09674e7b3d
Vlad Ghinea <vlad@ghn.me>
2015-12-05 10:47:24 -0600

    Add command line arguments to bootstrap script
    - Add option to get instance and ami ids
    - Add option to create a CSR attributes file with role and ids
    - Support only Linux machines

--- bin/bootstrap_agent.sh
+++ bin/bootstrap_agent.sh
@@ -3,17 +3,39 @@
 # VGH bootstrap script
 #
 # USAGE:
-# bash <(wget -qO- https://raw.githubusercontent.com/vladgh/puppet/master/bin/bootstrap_agent.sh)
-
-# DEFAULTS
-COLLECTION='pc1'
-
-# Wrap the entire script inside a function to make sure that, when piped, it's
-# retrieved completely
-bootstrap(){
+# git clone https://github.com/vladgh/puppet.git
+# cd puppet
+# bash bin/bootstrap_agent.sh [args]
+#
+# Command line arguments:
+# --role=myrole -> declares the role of the node as a trusted fact*
+# --aws -> adds the instance and ami ids as a trusted fact (requires role)
+#
+# * Trusted facts info: https://docs.puppetlabs.com/puppet/latest/reference/lang_facts_and_builtin_vars.html#trusted-facts
 
 # Immediately exit on errors
 set -euo pipefail
+
+# DEFAULTS
+export PUPPET_MASTER='puppet.vladgh.com'
+
+# Parse command line arguments
+# defaults
+ROLE='none'
+AWS=false
+# cli
+for var in "$@"; do
+  if [[ "$var" =~ --role=.* ]]; then
+    ROLE=${var//--role=/}
+  elif [[ "$var" =~ --aws ]]; then
+    AWS=true
+  fi
+done
+
+# Check if it's a Linux machine
+if [[ $(uname) != Linux ]]; then
+  echo 'FATAL: Only Linux is supported'; exit 1
+fi
 
 # FUNCTIONS
 # Check if command exists
@@ -26,6 +48,8 @@ apt_install(){ echo "Installing $*" && sudo apt-get -qy install "$@" < /dev/null
 apt_update() { echo 'Updating APT' && sudo apt-get -qy update < /dev/null ;}
 
 # VARs
+# The Puppet collection
+PUPPET_COLLECTION='pc1'
 # Allow overriding the distribution codename
 CODENAME=${CODENAME:-$(get_release)}
 # Create temporary directory
@@ -33,11 +57,11 @@ TEMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmp')
 
 # Install Puppet release package
 if [[ ! -x /opt/puppetlabs/bin/puppet ]] ; then
-  debname="puppetlabs-release-${COLLECTION}-${CODENAME}.deb"
+  debname="puppetlabs-release-${PUPPET_COLLECTION}-${CODENAME}.deb"
   debfile="${TEMPDIR}/${debname}"
   if is_cmd wget; then
     if ! wget -qO "$debfile" "https://apt.puppetlabs.com/${debname}"; then
-      echo 'Could not download the release package'; exit 1
+      echo 'FATAL: Could not download the release package'; exit 1
     fi
   else
     apt_update && apt_install wget
@@ -47,14 +71,33 @@ if [[ ! -x /opt/puppetlabs/bin/puppet ]] ; then
     sudo dpkg -i "$debfile" && rm "$debfile"
     apt_update && apt_install puppet-agent
   else
-    echo 'Could not install Puppet release package'; exit 1
+    echo 'FATAL: Could not install Puppet release package'; exit 1
   fi
 else
-  echo 'Puppet is already installed.'
+  echo 'WARNING: Puppet is already installed.'
 fi
 
-} # end bootstrap function
-
-# RUN
-bootstrap "$@"
+# Write CSR attributes to file
+if [ "$ROLE" != 'none' ]; then
+  echo 'Creating a CSR Attributes file'
+  # Ensure directory is present
+  [ -d /etc/puppetlabs/puppet ] && mkdir -p /etc/puppetlabs/puppet
+  # Start by writing the role
+  cat > /etc/puppetlabs/puppet/csr_attributes.yaml << YAML
+extension_requests:
+  pp_role: $PP_ROLE
+YAML
+  # Get Instance and AMI IDs
+  if [ "$AWS" = true ]; then
+    # Get AWS instance ID
+    AWS_INSTANCE_ID="$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
+    # Get AWS image ID
+    AWS_AMI_ID="$(curl -s http://169.254.169.254/latest/meta-data/ami-id)"
+    # Append to the previous file
+    cat >> /etc/puppetlabs/puppet/csr_attributes.yaml << YAML
+  pp_instance_id: $AWS_INSTANCE_ID
+  pp_image_name: $AWS_AMI_ID
+YAML
+  fi
+fi
 
