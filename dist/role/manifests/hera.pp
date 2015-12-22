c7c8bc8fe27ce826a4754dbd6ac75df2e342f75c
Vlad Ghinea <vlad@ghn.me>
2015-12-22 14:12:36 -0600

    Add classes to the Hera and Zeus roles

--- dist/role/manifests/hera.pp
+++ dist/role/manifests/hera.pp
@@ -1,4 +1,7 @@
 # Hera Role (VGH AWS AMIs)
 class role::hera {
   include ::profile::base
+  include ::profile::ec2
+  include ::profile::docker
+  include ::profile::puppet::master
 }
--- dist/role/manifests/zeus.pp
+++ dist/role/manifests/zeus.pp
@@ -1,4 +1,7 @@
 # Zeus Role (VGH AWS On-Demand Instances)
 class role::zeus {
   include ::profile::base
+  include ::profile::ec2
+  include ::profile::docker
+  include ::profile::puppet::master
 }
