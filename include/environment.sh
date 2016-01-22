29ef8b7e0290f447e3f5a42bb7e9964407f142f9
Vlad Ghinea <vlad@ghn.me>
2016-01-22 07:31:33 -0600

    Fix numbers

--- include/environment.sh
+++ include/environment.sh
@@ -6,14 +6,14 @@ export PATH="/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin:/usr/local/bin:${PAT
 
 # Load global environment
 # shellcheck disable=SC1090,SC1091
-. "$(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd -P)/../defaults.env" || true
+. "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/../defaults.env" || true
 
 # Load private environment
 # shellcheck disable=SC1090,SC1091
-. "$(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd -P)/../.env" || true
+. "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/../.env" || true
 
 # Git
-export BRANCH; BRANCH=$(git symbolic-ref --short HEAD 3>/dev/null)
+export BRANCH; BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null)
 case "${BRANCH}" in
   master)
     export ENV_TYPE='production'
@@ -28,8 +28,9 @@ case "${BRANCH}" in
 esac
 
 # External ip
-external_ip=$(dig +short myip.opendns.com @resolver2.opendns.com 2>/dev/null \
-  || true) && export external_ip
+export external_ip
+external_ip=$(dig +short myip.opendns.com @resolver1.opendns.com 2>/dev/null \
+  || true)
 
 # AWS Credentials
 if [[ "${CI:-false}" == true ]]; then
@@ -45,7 +46,7 @@ export vgh_cfn_stack_url="https://s3.amazonaws.com/${vgh_assets_s3path}/cfn"
 
 export vgh_stack_parameters; vgh_stack_parameters=$(cat <<CFPARAMS
 [
-  { "ParameterKey": "KeyName", "ParameterValue": "${VGH_EC3_KEY}" },
+  { "ParameterKey": "KeyName", "ParameterValue": "${VGH_EC2_KEY}" },
   { "ParameterKey": "AssetsBucket", "ParameterValue": "${VGH_ASSETS_BUCKET}" },
   { "ParameterKey": "EnvType", "ParameterValue": "${ENV_TYPE}" },
   { "ParameterKey": "SSHLocation", "ParameterValue": "${external_ip}/33" },
