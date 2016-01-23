1de138e9a674d36007035a9357ada60bd4a160a9
Vlad Ghinea <vlad@ghn.me>
2016-01-23 13:17:19 -0600

    Fix S3 paths

--- include/environment.sh
+++ include/environment.sh
@@ -49,7 +49,8 @@ if [[ "${CI:-false}" == true ]]; then
 fi
 
 # AWS S3
-export AWS_ASSETS_S3_PREFIX="${VGH_ASSETS_BUCKET}/puppet/${ENV_TYPE}"
+export AWS_ASSETS_BUCKET="$VGH_ASSETS_BUCKET"
+export AWS_ASSETS_KEY_PREFIX="puppet/${ENV_TYPE}"
 
 # AWS CodeDeploy
 export CD_APP_NAME="$VGH_APPLICATION_NAME"
@@ -58,23 +59,23 @@ export CD_ARCHIVE="${CD_APP_NAME}-${VERSION}-${BUILD}.${CD_ARCHIVE_BUNDLE}"
 export CD_ARCHIVE_PATH="/tmp/${CD_ARCHIVE}"
 export CD_GROUP_NAME="${ENV_TYPE}"
 export CD_CONFIG='CodeDeployDefault.OneAtATime'
-export CD_BUCKET="${VGH_ASSETS_BUCKET}"
-export CD_KEY_PREFIX="${AWS_ASSETS_S3_PREFIX}/deploy"
+export CD_BUCKET="$AWS_ASSETS_BUCKET"
+export CD_KEY_PREFIX="${AWS_ASSETS_KEY_PREFIX}/deploy"
 export CD_KEY="${CD_KEY_PREFIX}/${CD_ARCHIVE}"
 export CD_S3_PATH="s3://${CD_BUCKET}/${CD_KEY}"
 
 # AWS CloudFormation
 export CFN_STACK_NAME="$VGH_STACK_NAME"
 export CFN_STACK_TEMPLATE="file://${APPDIR}/${VGH_STACK_FILE}"
-export CFN_STACK_S3="s3://${AWS_ASSETS_S3_PREFIX}/cfn"
-export CFN_STACK_URL="https://s3.amazonaws.com/${AWS_ASSETS_S3_PREFIX}/cfn"
+export CFN_STACK_S3="s3://${AWS_ASSETS_KEY_PREFIX}/cfn"
+export CFN_STACK_URL="https://s3.amazonaws.com/${AWS_ASSETS_KEY_PREFIX}/cfn"
 export CFN_STACK_CAPABILITIES='CAPABILITY_IAM'
 export CFN_STACK_ON_FAILURE='DELETE'
 
 export CFN_STACK_PARAMETERS; CFN_STACK_PARAMETERS=$(cat <<CFPARAMS
 [
   { "ParameterKey": "KeyName", "ParameterValue": "${VGH_EC2_KEY}" },
-  { "ParameterKey": "AssetsBucket", "ParameterValue": "${VGH_ASSETS_BUCKET}" },
+  { "ParameterKey": "AssetsBucket", "ParameterValue": "${AWS_ASSETS_BUCKET}" },
   { "ParameterKey": "EnvType", "ParameterValue": "${ENV_TYPE}" },
   { "ParameterKey": "SSHLocation", "ParameterValue": "${EXTERNAL_IP}/32" },
   { "ParameterKey": "VPCTemplateKey", "ParameterValue": "${CFN_STACK_URL}/vpc.json" },
