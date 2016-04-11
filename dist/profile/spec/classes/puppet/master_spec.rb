11d325c10f88e2b605dc9b37b03de9635ce3c151
Vlad <vlad@ghn.me>
2016-04-11 08:53:51 -0500

    Fix hiera call in template

--- dist/profile/spec/classes/puppet/master_spec.rb
+++ dist/profile/spec/classes/puppet/master_spec.rb
@@ -4,7 +4,7 @@ describe 'profile::puppet::master' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
-        let(:facts) { facts }
+        let(:facts) { facts.merge(aws_assets_bucket: 'my_bucket') }
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('profile::puppet::master') }
