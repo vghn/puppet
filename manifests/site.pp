909efb14c58d2bebfcaf9d9952d6eb1f5e59d907
Vlad <vlad@ghn.me>
2016-04-24 16:30:26 -0500

    Clean-up site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,20 +4,16 @@ Exec { path => '/usr/local/bin:/usr/bin:/usr/sbin/:/bin:/sbin' }
 # DEFAULT NODE
 node default {
   if $::trusted['authenticated'] == 'remote' {
-    if !empty( $::trusted['extensions']['pp_role'] ) {
-      info("Applying role '${::trusted['extensions']['pp_role']}'...")
-      include "::role::${::trusted['extensions']['pp_role']}"
-    } else {
-      fail('The \'pp_role\' trusted fact could not be found!')
-    }
+    $real_role = $::trusted['extensions']['pp_role']
   } elsif $::trusted['authenticated'] == 'local' {
-    if !empty( $::role ) {
-      notice("Applying role '${::role}'...")
-      include "::role::${::role}"
-    } else {
-      fail('The \'role\' fact could not be found!')
-    }
+    $real_role = $::role
   } else {
     fail('Unauthorized node!')
+  }
+
+  if !empty( $real_role ) {
+    include "::role::${::role}"
+  } else {
+    fail('The \'pp_role\' or the \'role\' fact could not be found!')
   }
 }
