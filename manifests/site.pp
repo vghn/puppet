f837c00b56261d4f41399605761fe83fd2498209
Vlad <vlad@ghn.me>
2016-04-26 15:37:22 -0500

    Move the real_role to top scope

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,19 @@
+# Role
+if $::trusted['authenticated'] == 'remote' {
+  $real_role = $::trusted['extensions']['pp_role']
+} elsif $::trusted['authenticated'] == 'local' {
+  $real_role = $::role
+} else {
+  fail('Unauthorized node!')
+}
+
 # Exec defaults:
 Exec { path => '/usr/local/bin:/usr/bin:/usr/sbin/:/bin:/sbin' }
 
 # DEFAULT NODE
 node default {
-  if $::trusted['authenticated'] == 'remote' {
-    $real_role = $::trusted['extensions']['pp_role']
-  } elsif $::trusted['authenticated'] == 'local' {
-    $real_role = $::role
-  } else {
-    fail('Unauthorized node!')
-  }
-
-  if !empty( $real_role ) {
-    include "::role::${real_role}"
+  if !empty( $::real_role ) {
+    include "::role::${::real_role}"
   } else {
     fail('The \'pp_role\' or the \'role\' fact could not be found!')
   }
