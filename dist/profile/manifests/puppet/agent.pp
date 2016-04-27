5e4df1ca735ea04948b6082d17416f02ea63e133
Vlad <vlad@ghn.me>
2016-04-27 14:52:14 -0500

    Run puppet agent twice per hour

--- dist/profile/manifests/puppet/agent.pp
+++ dist/profile/manifests/puppet/agent.pp
@@ -10,7 +10,7 @@ class profile::puppet::agent {
   cron {'Puppet Run':
     ensure  => present,
     user    => 'root',
-    minute  => fqdn_rand(60),
+    minute  => [fqdn_rand(30), fqdn_rand(30) + 30],
     command => '/opt/puppetlabs/bin/puppet agent --onetime --no-daemonize --logdest syslog > /dev/null 2>&1',
   }
 }
