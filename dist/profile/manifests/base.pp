199b80c58db22fa1fedfed78b232fda62986c448
Vlad <vlad@ghn.me>
2016-05-01 16:06:25 -0500

    Add logging container
    Send everything to Papertrail

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -32,8 +32,8 @@ class profile::base {
   }
 
   # Papertrail Logging
-  $log_server_address = hiera('log_server_address', undef)
-  $log_server_port = hiera('log_server_port', undef)
+  $log_server_address = hiera('LOG_SERVER', undef)
+  $log_server_port = hiera('LOG_PORT', undef)
   if ($log_server_port and $log_server_port) {
     class{'::rsyslog::client':
       remote_servers => [
