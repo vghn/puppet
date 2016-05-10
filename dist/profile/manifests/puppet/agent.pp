743eb2e41c61d5204d40e4f6125c2fa098e516fb
Vlad <vlad@ghn.me>
2016-05-10 16:31:31 -0500

    Use the trusted certname fact for agent cron job

--- dist/profile/manifests/puppet/agent.pp
+++ dist/profile/manifests/puppet/agent.pp
@@ -7,7 +7,7 @@ class profile::puppet::agent {
   }
 
   # Cron job to run puppet agent
-  $minute = fqdn_rand(30, $facts['certname'])
+  $minute = fqdn_rand(30, $trusted['certname'])
   cron {'Puppet Run':
     ensure  => present,
     command => 'sudo /opt/puppetlabs/bin/puppet agent --onetime --no-daemonize --logdest syslog > /dev/null 2>&1',
