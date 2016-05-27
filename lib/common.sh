58b2ea7cdaf7b4428aa55604bc77ec1d77f57897
Vlad <vlad@ghn.me>
2016-05-27 11:20:44 -0500

    Improve bootstrap
    Push an archive of the hiera data to S3 and allow the bootstrap script
    to download it.

--- lib/common.sh
+++ lib/common.sh
@@ -2,8 +2,45 @@
 # Common tasks
 
 # Download private data
-upload_hieradata(){
+sync_hieradata(){
   e_info 'Sync hiera data'
   aws s3 sync --delete \
-    "${APPDIR}/hieradata" "$HIERADATA_S3"
+    "${APPDIR}/hieradata" "$PP_HIERADATA_S3"
+}
+
+# Archive Puppet Control Repo
+archive_hieradata(){
+  hieradata_archive_path="${TMPDIR}/hieradata.tgz"
+
+  # Only pack the required files
+  e_info "Creating hieradata archive (${hieradata_archive_path})"
+  if ! tar czf "$hieradata_archive_path" \
+    bin/ dist/{profile,role}/{manifests,templates} hieradata/ lib/ manifests/ \
+    .env bootstrap environment.conf envrc hiera.yaml Puppetfile r10k.yaml \
+    CHANGELOG.md LICENSE README.md VERSION;
+  then
+    e_abort "Could not create ${hieradata_archive_path}"
+  fi
+}
+
+# Upload archive to S3
+archive_upload(){
+  e_info "Uploading hieradata archive (${PP_HIERADATA_ARCHIVE_S3PATH})"
+  if ! aws s3 cp "$hieradata_archive_path" "$PP_HIERADATA_ARCHIVE_S3PATH" --quiet; then
+    e_abort "Could not upload ${hieradata_archive_path} to ${PP_HIERADATA_ARCHIVE_S3PATH}"
+  fi
+}
+
+# Clean-up
+archive_clean(){
+  e_info "Cleaning-up hieradata archive ${hieradata_archive_path}"
+  rm "$hieradata_archive_path" || true
+}
+
+# Upload app
+upload_hieradata(){
+  sync_hieradata
+  archive_hieradata
+  archive_upload
+  archive_clean
 }
