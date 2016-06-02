bd5db09874935a24b9bec67ed4b94d4664016419
Vlad <vlad@ghn.me>
2016-06-02 10:13:43 -0500

    Name R10K container

--- lib/ci.sh
+++ lib/ci.sh
@@ -28,7 +28,7 @@ ci_deploy(){
   aws_ec2_send_run_command \
   'zeus' \
   'Deploy R10K environment' \
-  "docker run --rm \
+  "docker run --rm --name r10k-agent \
     -v /opt/vpm/puppet/r10k.yaml:/etc/puppetlabs/r10k/r10k.yaml:ro \
     -v /opt/vpm/puppet/bin/r10k-post-run:/etc/puppetlabs/r10k/post-run.sh:ro \
     -v /opt/vpm/code/environments:/etc/puppetlabs/code/environments \
