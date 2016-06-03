ff393544b8bb5086529a8e49c04a7a75cc39d3bc
Vlad <vlad@ghn.me>
2016-06-03 13:36:44 -0500

    Move r10k post run hook and csr sign to roles/zeus
    Clean-up profiles and roles

--- dist/role/manifests/zeus.pp
+++ dist/role/manifests/zeus.pp
@@ -10,4 +10,39 @@ class role::zeus {
   include ::profile::aws::s3fs
   include ::profile::docker
   include ::profile::rvm
+
+  # VARs
+  $project_path = hiera(project_path, "/opt/${real_role}")
+
+  # Docker Compose File
+  $log_server_address = hiera('log_server_address', undef)
+  $log_server_port    = hiera('log_server_port', undef)
+  file {'Docker Compose':
+    ensure  => present,
+    path    => "${project_path}/docker-compose.yaml",
+    content => template('role/zeus/docker-compose.yaml.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
+  # R10k Post Run Hook
+  file {'R10k Post Run Hook':
+    ensure => present,
+    path   => "${project_path}/scripts/r10k-post-run",
+    source => 'puppet:///modules/role/zeus/r10k-post-run',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  # Policy based CSR auto sign script
+  file {'CSR Auto Sign':
+    ensure => present,
+    path   => "${project_path}/scripts/csr-sign",
+    source => 'puppet:///modules/role/zeus/csr-sign',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0555',
+  }
 }
