d3bd6a6449750903e96e8b5b54ddddfbbd1c1263
Vlad <vlad@ghn.me>
2016-07-18 12:21:00 -0500

    Improve Rhea upstart script

--- dist/profile/manifests/puppet/master.pp
+++ dist/profile/manifests/puppet/master.pp
@@ -3,7 +3,7 @@ class profile::puppet::master {
   # Upstart script for Rhea
   file { '/etc/init/rhea.conf' :
     ensure => file,
-    source => 'puppet:///modules/profile/rhea/upstart-rhea.conf',
+    source => 'puppet:///modules/profile/rhea/init/rhea.conf',
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
