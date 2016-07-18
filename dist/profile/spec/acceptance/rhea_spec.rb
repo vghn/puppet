d603eea709d43f600f3068091b275e2f46f3e14f
Vlad <vlad@ghn.me>
2016-07-18 14:14:50 -0500

    Add RVM to RHEA

--- dist/profile/spec/acceptance/rhea_spec.rb
+++ dist/profile/spec/acceptance/rhea_spec.rb
@@ -10,6 +10,7 @@ describe 'Rhea role', if: hosts.map(&:name).include?('rhea') do
       include ::profile::python
       include ::profile::git
       include ::profile::jq
+      include ::profile::rvm
       include ::profile::puppet::agent
       include ::profile::puppet::master
       include ::profile::aws::cloudformation
@@ -28,6 +29,7 @@ describe 'Rhea role', if: hosts.map(&:name).include?('rhea') do
   it_behaves_like 'profile::python'
   it_behaves_like 'profile::git'
   it_behaves_like 'profile::jq'
+  it_behaves_like 'profile::rvm'
   it_behaves_like 'profile::puppet::agent'
   it_behaves_like 'profile::puppet::master'
   it_behaves_like 'profile::aws::cloudformation'
--- dist/role/manifests/rhea.pp
+++ dist/role/manifests/rhea.pp
@@ -6,6 +6,7 @@ class role::rhea {
   include ::profile::python
   include ::profile::git
   include ::profile::jq
+  include ::profile::rvm
   include ::profile::puppet::master
   include ::profile::puppet::agent
   include ::profile::aws::cloudformation
