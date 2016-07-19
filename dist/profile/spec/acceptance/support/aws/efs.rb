5df9716c1b609a4cce35d44607c554c36a239531
Vlad <vlad@ghn.me>
2016-07-19 15:21:02 -0500

    Add AWS EFS profile

---
+++ dist/profile/spec/acceptance/support/aws/efs.rb
@@ -0,0 +1,9 @@
+shared_examples 'profile::aws::efs' do
+  describe file('/etc/fstab') do
+    its(:content) { is_expected.to match(/fs-1234/) }
+  end
+
+  describe command('mountpoint -q /efs') do
+    its(:exit_status) { should eq 0 }
+  end
+end
