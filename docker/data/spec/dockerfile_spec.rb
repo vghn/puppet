9bae2f481ceb998a7273e914a2a6dad3bb874b52
Vlad <vlad@ghn.me>
2016-08-01 11:18:03 -0500

    Fix spec tests
    For data container

--- docker/data/spec/dockerfile_spec.rb
+++ docker/data/spec/dockerfile_spec.rb
@@ -17,23 +17,19 @@ describe 'Dockerfile' do
     it { is_expected.to be_installed.by('gem') }
   end
 
-  describe file('/etc/puppetlabs/r10k/r10k.yaml') do
-    it { should exist }
+  describe command('aws --version') do
+    its(:stderr) { should contain('aws-cli') }
   end
 
-  describe 'Dockerfile#running' do
-    include_context 'with a docker container'
+  describe command('git version') do
+    its(:stdout) { should contain('git') }
+  end
 
-    describe command('aws --version') do
-      its(:stderr) { should contain('aws-cli') }
-    end
+  describe command('r10k version') do
+    its(:stdout) { should contain('r10k') }
+  end
 
-    describe command('git version') do
-      its(:stdout) { should contain('git') }
-    end
-
-    describe command('r10k version') do
-      its(:stdout) { should contain('r10k') }
-    end
+  describe file('/etc/puppetlabs/r10k/r10k.yaml') do
+    it { should exist }
   end
 end
