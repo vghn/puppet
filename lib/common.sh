16ec408506558437ba51b8892a53f41f4e313e93
Vlad <vlad@ghn.me>
2016-08-10 16:36:35 -0500

    Fix deployment

--- lib/common.sh
+++ lib/common.sh
@@ -12,16 +12,16 @@ set_bundle_directory(){
 
 upload_private_data(){
   e_info 'Upload .env'
-  if ! aws s3 cp "${APPDIR}/.env" "${ENV_S3PATH}" --quiet; then
-    e_abort "Could not upload ${APPDIR}/.env to ${ENV_S3PATH}"
+  if ! aws s3 cp "${APPDIR}/.env" "${ENV_S3PATH}/.${ENVTYPE}" --quiet; then
+    e_abort "Could not upload ${APPDIR}/.env to ${ENV_S3PATH}/.${ENVTYPE}"
   fi
   e_info 'Upload vault'
-  if ! aws s3 sync "${APPDIR}/vault/" "${VAULT_S3PATH}/" --delete; then
-    e_abort "Could not upload ${APPDIR}/vault/ to ${VAULT_S3PATH}"
+  if ! aws s3 sync "${APPDIR}/vault/" "${VAULT_S3PATH}/${ENVTYPE}/" --delete; then
+    e_abort "Could not upload ${APPDIR}/vault/ to ${VAULT_S3PATH}/${ENVTYPE}/"
   fi
   e_info 'Upload hieradata'
-  if ! aws s3 sync "${APPDIR}/hieradata/" "${HIERA_S3PATH}/" --delete; then
-    e_abort "Could not upload ${APPDIR}/hieradata/ to ${VAULT_S3PATH}"
+  if ! aws s3 sync "${APPDIR}/hieradata/" "${HIERA_S3PATH}/${ENVTYPE}/" --delete; then
+    e_abort "Could not upload ${APPDIR}/hieradata/ to ${HIERA_S3PATH}/${ENVTYPE}/"
   fi
 }
 
@@ -32,11 +32,11 @@ download_private_data(){
   fi
   e_info 'Download vault'
   if ! aws s3 sync "${VAULT_S3PATH}/" "${APPDIR}/vault/" --delete; then
-    e_abort "Could not download ${VAULT_S3PATH} to ${APPDIR}/vault"
+    e_abort "Could not download ${VAULT_S3PATH}/ to ${APPDIR}/vault/"
   fi
   e_info 'Download hieradata'
   if ! aws s3 sync "${HIERA_S3PATH}/" "${APPDIR}/hieradata/" --delete; then
-    e_abort "Could not download ${VAULT_S3PATH} to ${APPDIR}/hieradata"
+    e_abort "Could not download ${VAULT_S3PATH}/ to ${APPDIR}/hieradata/"
   fi
 }
 
