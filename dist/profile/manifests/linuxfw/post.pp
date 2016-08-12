a3dc45900cfd21428bc938f9ddb3f68b6b809500
Vlad <vlad@ghn.me>
2016-08-12 15:14:35 -0500

    Firewall should end by dropping input and forward

--- dist/profile/manifests/linuxfw/post.pp
+++ dist/profile/manifests/linuxfw/post.pp
@@ -1,6 +1,12 @@
 # Firewall Profile (After)
 class profile::linuxfw::post {
-  firewall { '999 drop all':
+  firewall { '998 drop all forward':
+    proto  => 'all',
+    chain  => 'FORWARD',
+    action => 'drop',
+    before => undef,
+  }
+  firewall { '999 drop all input':
     proto  => 'all',
     action => 'drop',
     before => undef,
