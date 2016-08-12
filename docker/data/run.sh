8053c8e7361b6588f4f9c58627c841a300a5cd4e
Vlad <vlad@ghn.me>
2016-08-12 18:27:18 -0500

    Allow specifying the SSL Certificate

--- docker/data/run.sh
+++ docker/data/run.sh
@@ -4,14 +4,21 @@
 . /opt/vpm/envrc
 
 # Generate self signed certificate
-if [[ ! -s /var/local/key.pem ]]; then
-  (
-  cd /var/local || exit
-  openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes -subj '/CN=*/O=VGH/C=US'
-  )
+if [[ -z "${RACK_SSL_KEY:-}" ]] &&  [[ -z "${RACK_SSL_CRT:-}" ]]; then
+  if [[ ! -s /var/local/key.pem ]]; then
+    export RACK_SSL_KEY=/var/local/key.pem
+    export RACK_SSL_CRT=/var/local/crt.pem
+    openssl req -x509 \
+      -newkey rsa:2048 \
+      -keyout "$RACK_SSL_KEY" \
+      -out "$RACK_SSL_CRT" \
+      -days 365 \
+      -nodes \
+      -subj '/CN=*/O=VGH/C=US'
+  fi
 fi
 
 # Start server
 bundle exec puma \
   --environment "$RACK_ENV" \
-  --bind "ssl://0.0.0.0:${RACK_PORT}?key=/var/local/key.pem&cert=/var/local/cert.pem"
+  --bind "ssl://0.0.0.0:${RACK_PORT}?key=${RACK_SSL_KEY}&cert=${RACK_SSL_CRT}"
