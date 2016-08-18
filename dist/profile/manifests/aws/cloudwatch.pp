825ca5235d466eb4f85252b4d0d0a4b2ded884ac
Vlad <vlad@ghn.me>
2016-08-18 10:21:05 -0500

    Improve profiles

--- dist/profile/manifests/aws/cloudwatch.pp
+++ dist/profile/manifests/aws/cloudwatch.pp
@@ -1,9 +1,13 @@
 # AWS CloudWatch Logs
 class profile::aws::cloudwatch {
-  $aws_cloudwatch_logs = hiera_hash('aws_cloudwatch_logs', undef)
-  if ($aws_cloudwatch_logs != undef) {
-    class { '::cloudwatchlogs': region => 'us-east-1' }
-    Concat['/etc/awslogs/awslogs.conf'] -> Exec['cloudwatchlogs-install']
-    create_resources(cloudwatchlogs::log, $aws_cloudwatch_logs)
+
+  class { '::cloudwatchlogs': region => 'us-east-1' }
+  Concat['/etc/awslogs/awslogs.conf'] -> Exec['cloudwatchlogs-install']
+
+  $logs = hiera_hash('profile::aws::cloudwatch::logs', {})
+  $logs.each |String $name, Hash $params| {
+    cloudwatchlogs::log { $name:
+      * => $params;
+    }
   }
 }
--- dist/profile/manifests/jq.pp
+++ dist/profile/manifests/jq.pp
@@ -1,10 +1,10 @@
-# JQ Profile
+# JQ JSON Processor Profile
 class profile::jq(String $version) {
-  # JQ JSON Processor
   wget::fetch {'JQ JSON Processor':
     source      => "https://github.com/stedolan/jq/releases/download/jq-${version}/jq-linux64",
     destination => '/usr/local/bin/jq',
   }
+
   file {'/usr/local/bin/jq':
     owner   => 'root',
     group   => 'root',
