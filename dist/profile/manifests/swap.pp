bda6aec0d3af97554a2ef3dc70ea67c27942cf7d
Vlad <vlad@ghn.me>
2016-08-20 09:24:36 -0500

    Add petems/swap_file module
    Closes #24

--- dist/profile/manifests/swap.pp
+++ dist/profile/manifests/swap.pp
@@ -1,24 +1,4 @@
 # SWAP Class
-class profile::swap(
-  String $swapfile = '/var/swap.space',
-  Integer $swapfile_size_mb = 1024,
-) {
-
-  exec {'Create swap file':
-    command => "/bin/dd if=/dev/zero of=${swapfile} bs=1M count=${swapfile_size_mb}",
-    creates => $swapfile,
-  }
-  exec {'Attach swap file':
-    command => "/sbin/mkswap ${swapfile} && /sbin/swapon ${swapfile}",
-    require => Exec['Create swap file'],
-    unless  => "/sbin/swapon -s | grep ${swapfile}",
-  }
-  mount {'swap':
-    ensure  => present,
-    fstype  => swap,
-    device  => $swapfile,
-    dump    => 0,
-    pass    => 0,
-    require => Exec['Attach swap file'],
-  }
+class profile::swap {
+  include ::swap_file
 }
