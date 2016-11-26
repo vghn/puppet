097df46e30703c945976fc0e3717ceb0826bfab6
Vlad <vlad@ghn.me>
2016-11-26 14:21:30 -0600

    Add exact time stamps to aws sync commands

--- lib/common.sh
+++ lib/common.sh
@@ -11,11 +11,11 @@ upload_private_data(){
     e_abort "Could not upload ${APPDIR}/.env to ${ENV_S3PATH}/.${ENVTYPE}"
   fi
   e_info 'Upload vault'
-  if ! aws s3 sync "${APPDIR}/vault/" "${VAULT_S3PATH}/${ENVTYPE}/" --delete; then
+  if ! aws s3 sync "${APPDIR}/vault/" "${VAULT_S3PATH}/${ENVTYPE}/" --delete --exact-timestamps; then
     e_abort "Could not upload ${APPDIR}/vault/ to ${VAULT_S3PATH}/${ENVTYPE}/"
   fi
   e_info 'Upload hieradata'
-  if ! aws s3 sync "${APPDIR}/hieradata/" "${HIERA_S3PATH}/${ENVTYPE}/" --delete; then
+  if ! aws s3 sync "${APPDIR}/hieradata/" "${HIERA_S3PATH}/${ENVTYPE}/" --delete --exact-timestamps; then
     e_abort "Could not upload ${APPDIR}/hieradata/ to ${HIERA_S3PATH}/${ENVTYPE}/"
   fi
 }
@@ -26,7 +26,7 @@ download_private_data(){
     e_abort "Could not download ${ENV_S3PATH}/.${ENVTYPE} to ${APPDIR}/.env"
   fi
   e_info 'Download vault'
-  if ! aws s3 sync "${VAULT_S3PATH}/${ENVTYPE}/" "${APPDIR}/vault/" --delete; then
+  if ! aws s3 sync "${VAULT_S3PATH}/${ENVTYPE}/" "${APPDIR}/vault/" --delete --exact-timestamps; then
     e_abort "Could not download ${VAULT_S3PATH}/${ENVTYPE}/ to ${APPDIR}/vault/"
   fi
   e_info 'Download hieradata'
