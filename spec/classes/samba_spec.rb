b66ca08efae248dadf0bd5b411263b6462b54a51
Vlad <vlad@ghn.me>
2017-02-25 12:06:11 -0600

    Add samba tests

--- spec/classes/samba_spec.rb
+++ spec/classes/samba_spec.rb
@@ -9,6 +9,11 @@ describe 'profile::samba' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('profile::samba') }
         it { is_expected.to contain_class('samba::server') }
+
+        it { is_expected.to contain_group('sambashare') }
+        it { is_expected.to contain_file('/data/test_share') }
+        it { is_expected.to contain_exec('mkdir_p-/data/test_share') }
+        it { is_expected.to contain_profile__mkdir_p('/data/test_share') }
       end
     end
   end
