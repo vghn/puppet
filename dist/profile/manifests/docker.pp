f8cb091c098a3eaf34a6a580414b701f35d137b4
Vlad <vlad@ghn.me>
2017-07-04 08:21:31 -0500

    Fix docker tests

--- dist/profile/manifests/docker.pp
+++ dist/profile/manifests/docker.pp
@@ -4,26 +4,32 @@ class profile::docker {
   if $::virtual == 'docker' {
     warning('Docker in Docker is not yet supported!')
   } else {
-    apt::key { 'docker':
-      source => 'https://download.docker.com/linux/ubuntu/gpg',
-      id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+    # Fix until the docker module supports the latest repository and package
+    if $facts['os']['name'] == 'Ubuntu' {
+      require ::apt
+
+      apt::key { 'docker':
+        source => 'https://download.docker.com/linux/ubuntu/gpg',
+        id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+      }
+
+      apt::source { 'docker':
+        location => 'https://download.docker.com/linux/ubuntu',
+        repos    => 'stable',
+        release  => "ubuntu-${facts['os']['distro']['codename']}",
+        require  => Apt::Key['docker'],
+      }
+
+      package { 'docker-ce':
+        ensure  => present,
+        require => [Apt::Source['docker'], Class['apt::update']],
+      }
+
+      package { ['docker.io','docker-engine']:
+        ensure => absent,
+      }
     }
 
-    apt::source { 'docker':
-      location => 'https://download.docker.com/linux/ubuntu',
-      repos    => 'stable',
-      release  => $facts['os']['distro']['codename'],
-      require  => Apt::Key['docker'],
-    }
-
-    package { 'docker-ce':
-      ensure  => present,
-      require => [Apt::Source['docker'], Class['apt::update']],
-    }
-
-    package { ['docker.io','docker-engine']:
-      ensure => absent,
-    }
     # # Docker main class
     # class { '::docker':
       # manage_kernel               => false,
