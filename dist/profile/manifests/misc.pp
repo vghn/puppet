127b62f6174e4d14d7aa80f7204dacaf48fbd6c1
Vlad <vlad@ghn.me>
2017-07-28 08:22:04 -0500

    Fix tests

--- dist/profile/manifests/misc.pp
+++ dist/profile/manifests/misc.pp
@@ -5,11 +5,11 @@ class profile::misc(
   # Do nothing when closing the lid on a laptop
   if $ignore_lid_switch {
     ini_setting { 'Ignore LID close':
-      ensure  => present,
-      path    => '/etc/systemd/logind.conf',
-      section => 'Login',
-      setting => 'HandleLidSwitch',
-      value   => 'ignore',
+      ensure            => present,
+      path              => '/etc/systemd/logind.conf',
+      section           => 'Login',
+      setting           => 'HandleLidSwitch',
+      value             => 'ignore',
       key_val_separator => '=',
     }
     ~> service { 'systemd-logind' :
