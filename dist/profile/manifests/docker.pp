3b390cbfdbb0a2a9324c4e30bdda470a2e24b8f8
Vlad <vlad@ghn.me>
2017-08-05 12:21:05 -0500

    Continue to use garethr-docker repo
    Closes #174

--- dist/profile/manifests/docker.pp
+++ dist/profile/manifests/docker.pp
@@ -6,62 +6,46 @@ class profile::docker {
   } else {
     # Fix until the docker module supports the latest repository and package
     if $facts['os']['name'] == 'Ubuntu' {
-      require ::apt
-
-      apt::key { 'docker':
-        source => 'https://download.docker.com/linux/ubuntu/gpg',
-        id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
-      }
-
-      apt::source { 'docker':
-        location     => 'https://download.docker.com/linux/ubuntu',
-        architecture => 'amd64',
-        repos        => 'stable',
-        release      => $facts['os']['distro']['codename'],
-        require      => Apt::Key['docker'],
-      }
-
-      package { 'docker-ce':
-        ensure  => present,
-        require => [Apt::Source['docker'], Class['apt::update']],
-      }
-
-      package { ['docker.io','docker-engine']:
-        ensure => absent,
+      # Docker main class
+      class { '::docker':
+        package_name                => 'docker-ce',
+        package_release             => $facts['os']['distro']['codename'],
+        package_repos               => 'stable',
+        package_source_location     => '[arch=amd64] https://download.docker.com/linux/ubuntu',
+        package_key_source          => 'https://download.docker.com/linux/ubuntu/gpg',
+        package_key                 => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+        manage_kernel               => false,
+        pin_upstream_package_source => false,
+        docker_command              => 'dockerd',
+        daemon_subcommand           => '',
       }
     }
 
-    # # Docker main class
-    # class { '::docker':
-      # manage_kernel               => false,
-      # pin_upstream_package_source => false,
-    # }
+    # Docker Compose
+    class { '::docker::compose':
+      version => '1.15.0',
+    }
 
-    # # Docker Compose
-    # class { '::docker::compose':
-      # version => '1.14.0',
-    # }
+    # Pull images
+    lookup({
+      'name'          => 'profile::docker::images',
+      'merge'         => 'hash',
+      'default_value' => {}
+    }).each |String $name, Hash $params| {
+      docker::image { $name:
+        * => $params;
+      }
+    }
 
-    # # Pull images
-    # lookup({
-      # 'name'          => 'profile::docker::images',
-      # 'merge'         => 'hash',
-      # 'default_value' => {}
-    # }).each |String $name, Hash $params| {
-      # docker::image { $name:
-        # * => $params;
-      # }
-    # }
-
-    # # Run containers
-    # lookup({
-      # 'name'          => 'profile::docker::run',
-      # 'merge'         => 'hash',
-      # 'default_value' => {}
-    # }).each |String $name, Hash $params| {
-      # docker::run { $name:
-        # * => $params;
-      # }
-    # }
+    # Run containers
+    lookup({
+      'name'          => 'profile::docker::run',
+      'merge'         => 'hash',
+      'default_value' => {}
+    }).each |String $name, Hash $params| {
+      docker::run { $name:
+        * => $params;
+      }
+    }
   }
 }
