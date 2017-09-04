65cd468f11f07b0b92c0de64db0bf2060117d4ad
Vlad <vlad@ghn.me>
2017-09-04 13:56:23 -0500

    Install NFS package
    Closes #181

--- dist/profile/manifests/aws/efs.pp
+++ dist/profile/manifests/aws/efs.pp
@@ -1,18 +1,18 @@
 # AWS Elastic File Storage Class
 class profile::aws::efs (
-  Optional[String] $mount_target = undef,
   Optional[String] $mount_point = undef,
+  Optional[String] $mount_target = undef,
 ) {
-  if $mount_target {
-    case $facts['os']['family'] {
-      'Debian': {
-        ensure_packages(['nfs-common'])
-      }
-      default: {
-        fail("The ${module_name} module is not supported on an ${facts['os']['family']} distribution.")
-      }
+  case $facts['os']['family'] {
+    'Debian': {
+      ensure_packages(['nfs-common'])
     }
+    default: {
+      fail("The ${module_name} module is not supported on an ${facts['os']['family']} distribution.")
+    }
+  }
 
+  if $mount_point and $mount_target {
     profile::mkdir_p { $mount_point: }
     -> mount { $mount_point:
       ensure  => present,
