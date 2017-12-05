d08f9e04aca3f8135f7d70c9e1605c173dea29c5
Vlad <vlad@ghn.me>
2017-12-05 10:05:09 -0600

    Fix IPTables integration tests
    Closes #191

--- spec/acceptance/support/profiles/linuxfw.rb
+++ spec/acceptance/support/profiles/linuxfw.rb
@@ -1,18 +1,10 @@
 shared_examples 'profile::linuxfw' do
   describe iptables do
     it do
-      is_expected.to have_rule(
-        '-A INPUT ' \
-        '-m comment --comment "003 accept related established rules" ' \
-        '-m state --state RELATED,ESTABLISHED -j ACCEPT'
-      )
+      is_expected.to have_rule('-A INPUT -m state --state RELATED,ESTABLISHED -m comment --comment "003 accept related established rules" -j ACCEPT')
     end
     it do
-      is_expected.to have_rule(
-        '-A INPUT -p tcp -m multiport --dports 1234 ' \
-        '-m comment --comment "123 test rule" ' \
-        '-m state --state NEW -j ACCEPT'
-      )
+      is_expected.to have_rule('-A INPUT -p tcp -m multiport --dports 1234 -m state --state NEW -m comment --comment "123 test rule" -j ACCEPT')
     end
   end
 end
