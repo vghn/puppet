f2b77d9e224648ef1e2a4dcf816aa40d0b9f4451
Vlad <vlad@ghn.me>
2017-12-19 07:42:50 -0600

    Minor changes to acceptance tests

--- gems.rb
+++ gems.rb
@@ -32,7 +32,7 @@ group :system_tests do
   gem 'beaker-puppet_install_helper', '~> 0.0', require: false
   gem 'beaker-rspec', '~> 6.0', require: false
   # SSH ED25519 support
+  gem 'bcrypt_pbkdf', '~> 1.0', require: false
   gem 'rbnacl', '~> 4.0', require: false
   gem 'rbnacl-libsodium', '~> 1.0', require: false
-  gem 'bcrypt_pbkdf', '~> 1.0', require: false
 end
