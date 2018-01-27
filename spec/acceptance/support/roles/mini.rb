741b27ed62fbef62617cb9a4457a42c9a93a8042
Vlad <vlad@ghn.me>
2018-01-27 14:50:06 -0600

    Fix acceptance tests for Mini

--- spec/acceptance/support/roles/mini.rb
+++ spec/acceptance/support/roles/mini.rb
@@ -2,6 +2,7 @@ shared_examples 'role::mini' do
   it_behaves_like 'profile::base'
   it_behaves_like 'profile::ca_certs'
   it_behaves_like 'profile::log'
+  it_behaves_like 'profile::python'
   it_behaves_like 'profile::git'
   it_behaves_like 'profile::puppet::agent'
   it_behaves_like 'profile::docker'
