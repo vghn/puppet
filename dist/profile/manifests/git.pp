4e2f8582dd8451d1fd737e48fa2a5d318a854137
Vlad <vlad@ghn.me>
2018-02-01 10:00:04 -0600

    Add option to use system git
    Closes #219

--- dist/profile/manifests/git.pp
+++ dist/profile/manifests/git.pp
@@ -1,13 +1,17 @@
 # GIT Profile
-class profile::git {
-  # Latest GIT
-  require ::apt
-  apt::ppa { 'ppa:git-core/ppa': package_manage => true }
-  class { '::git' :
-    package_ensure => 'latest',
-    require        => [
-      Apt::Ppa['ppa:git-core/ppa'],
-      Class['apt::update'],
-      ],
+class profile::git(Optional[Boolean] $system = false) {
+  if $system {
+    ensure_packages(['git'])
+  } else {
+    # Latest GIT
+    require ::apt
+    apt::ppa { 'ppa:git-core/ppa': package_manage => true }
+    class { '::git' :
+      package_ensure => 'latest',
+      require        => [
+        Apt::Ppa['ppa:git-core/ppa'],
+        Class['apt::update'],
+        ],
+    }
   }
 }
