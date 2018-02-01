435e4331be5689713c14f92411cfd70df602402e
Vlad <vlad@ghn.me>
2018-02-01 07:06:59 -0600

    Add VPI role
    Closes #213

---
+++ dist/role/manifests/vpi.pp
@@ -0,0 +1,13 @@
+# VPI Role (VGH Raspberry Pi)
+class role::vpi {
+  include ::profile::base
+  include ::profile::swap
+  include ::profile::ca_certs
+  include ::profile::log
+  include ::profile::monitor
+  include ::profile::python
+  include ::profile::git
+  include ::profile::jq
+  include ::profile::puppet::agent
+  include ::profile::docker
+}
