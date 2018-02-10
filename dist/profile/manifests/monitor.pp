d3241ec5c1507fd3f41f701d2dbae87dcff45194
Vlad <vlad@ghn.me>
2018-02-09 18:23:57 -0600

    Create the node exporter collector directory

--- dist/profile/manifests/monitor.pp
+++ dist/profile/manifests/monitor.pp
@@ -1,7 +1,13 @@
 # Monitor Class
-class profile::monitor {
+class profile::monitor (
+  Optional[String] $collector_directory = '/var/lib/node_exporter/textfile_collector'
+){
+  # Make sure directory exists
+
   # Node exporter class
-  class { 'prometheus::node_exporter':
-    version => '0.15.2',
+  profile::mkdir_p { $collector_directory: }
+  -> class { 'prometheus::node_exporter':
+    version       => '0.15.2',
+    extra_options => "--collector.textfile.directory ${collector_directory}"
   }
 }
--- spec/classes/monitor_spec.rb
+++ spec/classes/monitor_spec.rb
@@ -9,6 +9,9 @@ describe 'profile::monitor' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('profile::monitor') }
         it { is_expected.to contain_class('prometheus::node_exporter') }
+
+        it { is_expected.to contain_profile__mkdir_p('/var/lib/node_exporter/textfile_collector') }
+        it { is_expected.to contain_exec('mkdir_p-/var/lib/node_exporter/textfile_collector') }
       end
     end
   end
