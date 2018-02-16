b9b67eb0ad9fb2aae65d5bfc44138511c7ac7520
Vlad <vlad@ghn.me>
2018-02-16 07:29:07 -0600

    Fix node exporter ignore mount points
    Addresses #228

--- dist/profile/manifests/monitor.pp
+++ dist/profile/manifests/monitor.pp
@@ -14,6 +14,6 @@ class profile::monitor (
   # Node exporter class
   class { 'prometheus::node_exporter':
     version       => '0.15.2',
-    extra_options => "--collector.textfile.directory ${textfile_directory} --collector.filesystem.ignored-mount-points \"^/(sys|proc|dev|host|etc|rootfs/var/lib/docker/containers|rootfs/var/lib/docker/overlay2|rootfs/run/docker/netns|rootfs/var/lib/docker/aufs)($$|/)\""
+    extra_options => "--collector.textfile.directory ${textfile_directory} --collector.filesystem.ignored-mount-points \"^(/var/lib/docker|/run/docker)\""
   }
 }
