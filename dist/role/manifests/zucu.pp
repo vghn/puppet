a9754bf62cf0fde1bdc23232dce43c2fa7481601
Vlad <vlad@ghn.me>
2018-09-11 16:59:41 -0500

    Remove monitoring on internal servers

--- dist/role/manifests/zucu.pp
+++ dist/role/manifests/zucu.pp
@@ -3,7 +3,6 @@ class role::zucu {
   include ::profile::base
   include ::profile::ca_certs
   include ::profile::log
-  include ::profile::monitor
   include ::profile::python
   include ::profile::git
   include ::profile::jq
