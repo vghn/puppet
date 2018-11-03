6cc989608639b81fc2557cccedd41098834ebdb2
Vlad <vlad@ghn.me>
2018-11-03 16:50:42 -0500

    Fix deprecation in spec helper Closes #247

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+# Global RSpec configuration
+RSpec.configure do |config|
+  config.tty = true
+  config.color = true
+  config.fail_fast = true
+  config.formatter = :documentation
+  config.after(:suite) { RSpec::Puppet::Coverage.report!(95) }
+  config.mock_with :rspec
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 require 'rspec-puppet-facts'
@@ -7,14 +17,9 @@ fixtures_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 # RSpec configuration
 RSpec.configure do |config|
-  config.tty = true
-  config.color = true
-  config.fail_fast = true
-  config.formatter = :documentation
   config.trusted_node_data = true
   config.module_path = File.join(fixtures_path, 'modules/dist') + ':' +
                        File.join(fixtures_path, 'modules/r10k')
   config.manifest_dir = File.join(fixtures_path, 'manifests')
   config.hiera_config = File.join(fixtures_path, 'hiera.yaml')
-  config.after(:suite) { RSpec::Puppet::Coverage.report! }
 end
